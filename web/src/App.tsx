ddf37cfc1fc77a8c861cbc615c045054440d74ac
Fernando Pinto <fschultepinto@gmail.com>
2022-09-13 19:27:49 -0300

    Ajuste da sombra dentro do card

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -11,10 +11,10 @@ function App() {
     </h1>
 
     <div className='grid grid-cols-6 gap-6 mt-16'>
-      <a href="" className='relative'>
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/game-1.png"/>
 
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient'>
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
           <strong className='font-bold text-white block'>
             League of Legends
           </strong>
@@ -23,20 +23,65 @@ function App() {
           </span>
         </div>
       </a>
-      <a href="" className='relative'>
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/game-2.png"/>
+        
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white block'>
+            Dota 2
+          </strong>
+          <span className='text-zinc-300 text-sm block'>
+            4 Anúncios
+          </span>
+        </div>
       </a>
-      <a href="" className='relative'>
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/game-3.png"/>
+        
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white block'>
+            Counter-Strike
+          </strong>
+          <span className='text-zinc-300 text-sm block'>
+            4 Anúncios
+          </span>
+        </div>
       </a>
-      <a href="" className='relative'>
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/game-4.png"/>
+        
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white block'>
+            Apex Legends
+          </strong>
+          <span className='text-zinc-300 text-sm block'>
+            4 Anúncios
+          </span>
+        </div>
       </a>
-      <a href="" className='relative'>
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/game-5.png"/>
+        
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white block'>
+            Fortnite
+          </strong>
+          <span className='text-zinc-300 text-sm block'>
+            4 Anúncios
+          </span>
+        </div>
       </a>
-      <a href="" className='relative'>
+      <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/game-6.png"/>
+        
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white block'>
+            World of Warcraft
+          </strong>
+          <span className='text-zinc-300 text-sm block'>
+            4 Anúncios
+          </span>
+        </div>
       </a>
     </div>
   </div>
