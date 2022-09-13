c8d77009713849962189620a0f70c8277926122e
Fernando Pinto <fschultepinto@gmail.com>
2022-09-13 13:50:06 -0300

    Instalação do tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,9 @@
 import './styles.css';
 
-interface Buttonprops {
-  title: string;
-}
-
-function Button(props: Buttonprops) {
-  return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
-
 function App() {
   return (
   <div>
-    <Button title="Send 1" />
-    <Button title="Send 2" />
-    <Button title="Send 3" />
-
+    
   </div>
   )
 }
