490fdfcd9e01dab1f4dcd988bcd2c9c46fb9092f
Fernando Pinto <fschultepinto@gmail.com>
2022-09-14 00:44:53 -0300

    Configurando componente para img bgd

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,42 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { Background } from './src/components/Background';
 
-
-interface Buttonprops {
-  title: string;
-}
-
-function Button(props: Buttonprops){
-  return (
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Hello React Native!
-      </Text>
-      <Button title="Enviar" />
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+
+    </Background>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#000',
-    fontSize: 22
-  }
-});
