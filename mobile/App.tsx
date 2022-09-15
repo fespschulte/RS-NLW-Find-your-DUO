4ffb1ae0e0f755c9de971ca01b1ff2d498808dd3
Fernando Pinto <fschultepinto@gmail.com>
2022-09-14 23:42:18 -0300

    Criação de componentes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,29 @@
 import { StatusBar } from 'react-native';
 
+import { 
+  useFonts, 
+  Inter_400Regular, 
+  Inter_600SemiBold, 
+  Inter_700Bold, 
+  Inter_900Black 
+} from '@expo-google-fonts/inter';
+
 import { Background } from './src/components/Background';
+
+import { Home } from './src/screens/Home';
+
+import { Loading } from './src/components/Loading';
+
+
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black, 
+  })
   return (
     <Background>
         <StatusBar
@@ -11,7 +31,7 @@ export default function App() {
           backgroundColor="transpartent"
           translucent
         />
-
+      { fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
