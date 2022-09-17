8e978ee23c192ed5e44e4910052bc0ac8acf0da1
Fernando Pinto <fschultepinto@gmail.com>
2022-09-17 15:49:26 -0300

    Configuração da chamada dos cards pelo back

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -1,16 +1,22 @@
-export function GameBanner() {
-  return(
-<a href="" className='relative rounded-lg overflow-hidden'>
-        <img src="/game-1.png"/>
+interface GameBannerProps {
+  bannerUrl: string;
+  title: string;
+  adsCount: number;
+}
 
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>
-            League of Legends
-          </strong>
-          <span className='text-zinc-300 text-sm block'>
-            4 Anúncios
-          </span>
-        </div>
-      </a>
+export function GameBanner(props: GameBannerProps) {
+  return (
+    <a href="" className='relative rounded-lg overflow-hidden'>
+            <img src={props.bannerUrl}/>
+
+            <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>
+                {props.title}
+              </strong>
+              <span className='text-zinc-300 text-sm block'>
+                {props.adsCount} anúncio(s)
+              </span>
+            </div>
+          </a>
   )
 }
