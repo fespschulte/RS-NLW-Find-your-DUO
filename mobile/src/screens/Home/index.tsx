1754f9ccdd57568608482d50e02d228cdf9de640
Fernando Pinto <fschultepinto@gmail.com>
2022-09-18 11:12:28 -0300

    Instalação das dependências de React Navigation

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,15 +1,24 @@
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
+
 import { Heading } from '../../components/Heading';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 
 import { styles } from './styles';
 
-import { GAMES } from '../../utils/games';
-
-import { GameCard } from '../../components/GameCard';
-
 export function Home() {
+
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect (() => {
+    fetch('http://192.168.28.165:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image 
@@ -22,7 +31,7 @@ export function Home() {
       />
 
       <FlatList 
-        data={ GAMES }
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
           <GameCard 
