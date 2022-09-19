db1b29d68a8158ed714a382ce7c5cc8b3bd5e008
Fernando Pinto <fschultepinto@gmail.com>
2022-09-19 09:37:15 -0300

    Ajuste do select no form

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -1,6 +1,7 @@
-import * as Dialog from '@radix-ui/react-dialog'
+import * as Dialog from '@radix-ui/react-dialog';
+import * as Checkbox from '@radix-ui/react-checkbox';
 
-import { GameController } from 'phosphor-react';
+import { Check, GameController } from 'phosphor-react';
 import { Input } from './Form/Input';
 
 
@@ -14,7 +15,9 @@ export function CreateAdModal (){
           <form className="mt-8 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
             <label htmlFor="game" className="font-semibold">Qual o Game?</label>
-            <Input id="game" placeholder="Selecione o game que deseja jogar" />
+            <select className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500" id="game">
+              <option disabled selected>Selecione o game que deseja jogar</option>
+            </select>  
           </div>
           <div className="flex flex-col gap-2">
             <label htmlFor="name">Seu nome (ou nickname)</label>
@@ -88,8 +91,12 @@ export function CreateAdModal (){
                 </div>
             </div>
           </div>
-          <div className="mt-2 flex gap-2 text-sm">
-            <Input type="checkbox" />
+          <div className="mt-2 flex gap-2 items-center text-sm">
+            <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900">
+              <Checkbox.Indicator>
+                <Check className="w-4 h-4 text-emerald-400" />
+              </Checkbox.Indicator>
+            </Checkbox.Root>
             Costumo me conectar ao chat de voz
           </div>
           <footer className="mt-4 flex justify-end gap-4">
